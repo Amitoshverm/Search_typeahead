1faf9b9b5b83f15143c5000ae35c6e9d2164fa73
Amitoshverm <amitosh.verma9@gmail.com>
2026-04-29 22:01:33 +0530

    repo for searchTerm with custom methods

--- src/main/java/com/bookmarks/TypeAhead/repository/SearchTermRepository.java
+++ src/main/java/com/bookmarks/TypeAhead/repository/SearchTermRepository.java
@@ -1,4 +1,15 @@
 package com.bookmarks.TypeAhead.repository;
 
-public class SearchTermRepository {
+import com.bookmarks.TypeAhead.entity.SearchTerm;
+import com.bookmarks.TypeAhead.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SearchTermRepository extends JpaRepository<SearchTerm, Long> {
+
+    Optional<SearchTerm> findByWordAndUser(String word, Users user);
+    List<SearchTerm> findTop5ByUserOrderByFrequencyDesc(Users user);
+    Optional<SearchTerm> findByWord(String word);
 }

9e66b699bf30fd67fe3ae6f28de68296c3fd7937
Amitoshverm <amitosh.verma9@gmail.com>
2026-04-29 22:00:18 +0530

    model for searchTerm inside db

--- src/main/java/com/bookmarks/TypeAhead/entity/SearchTerm.java
+++ src/main/java/com/bookmarks/TypeAhead/entity/SearchTerm.java
@@ -1,4 +1,49 @@
 package com.bookmarks.TypeAhead.entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class SearchTerm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String word;
+    private int frequency;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Users user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+
+    public Users getUser() {
+        return user;
+    }
+
+    public void setUser(Users user) {
+        this.user = user;
+    }
 }

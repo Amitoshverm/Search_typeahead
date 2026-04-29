bf8328c8c9676e0c46e8211930f629438ba95aef
Amitoshverm <amitosh.verma9@gmail.com>
2026-04-29 21:59:47 +0530

    search controller

--- src/main/java/com/bookmarks/TypeAhead/controller/SearchController.java
+++ src/main/java/com/bookmarks/TypeAhead/controller/SearchController.java
@@ -1,4 +1,32 @@
 package com.bookmarks.TypeAhead.controller;
 
+import com.bookmarks.TypeAhead.service.SearchService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/search")
 public class SearchController {
+
+    private final SearchService searchService;
+
+    public SearchController(SearchService searchService) {
+        this.searchService = searchService;
+    }
+
+    @GetMapping("/suggest")
+    public ResponseEntity<List<String>> getSuggestions(
+            @RequestParam String prefix) {
+        List<String> suggestions = searchService.getSuggestions(prefix);
+        return new ResponseEntity<>(suggestions, HttpStatus.OK);
+    }
+
+    @PostMapping("/query")
+    public ResponseEntity<Void> searchWord(@RequestParam String word) {
+        searchService.searchWord(word);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }

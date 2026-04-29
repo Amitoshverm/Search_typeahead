9ef24ea8ae60c94ff799ae74ac74933714878a35
Amitoshverm <amitosh.verma9@gmail.com>
2026-04-29 22:03:51 +0530

    pre added words

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,14 @@
-INSERT IGNORE INTO search_term (word, frequency, last_searched_at, user_id) VALUES
-('apple', 10, NOW(), null),
-('application', 8, NOW(), null),
-('apply', 6, NOW(), null),
-('appetite', 3, NOW(), null),
-('apt', 2, NOW(), null),
-('ball', 9, NOW(), null),
-('batman', 7, NOW(), null),
-('band', 5, NOW(), null),
-('battle', 4, NOW(), null),
-('cat', 8, NOW(), null),
-('catch', 6, NOW(), null),
-('camera', 5, NOW(), null);
+INSERT IGNORE INTO search_term (word, frequency)
+VALUES ('apple', 10),
+       ('application', 8),
+       ('apply', 6),
+       ('appetite', 3),
+       ('apt', 2),
+       ('ball', 9),
+       ('batman', 7),
+       ('band', 5),
+       ('battle', 4),
+       ('cat', 8),
+       ('catch', 6),
+       ('camera', 5)
+ON DUPLICATE KEY UPDATE frequency = VALUES(frequency);

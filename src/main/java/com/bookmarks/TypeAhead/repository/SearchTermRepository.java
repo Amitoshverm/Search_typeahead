68fe0a0bbf20368e6cff444ecf96c4deed1453d6
Amitoshverm <amitosh.verma9@gmail.com>
2026-04-30 13:03:53 +0530

    annotation was missing

--- src/main/java/com/bookmarks/TypeAhead/repository/SearchTermRepository.java
+++ src/main/java/com/bookmarks/TypeAhead/repository/SearchTermRepository.java
@@ -3,10 +3,12 @@ package com.bookmarks.TypeAhead.repository;
 import com.bookmarks.TypeAhead.entity.SearchTerm;
 import com.bookmarks.TypeAhead.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface SearchTermRepository extends JpaRepository<SearchTerm, Long> {
 
     Optional<SearchTerm> findByWordAndUser(String word, Users user);

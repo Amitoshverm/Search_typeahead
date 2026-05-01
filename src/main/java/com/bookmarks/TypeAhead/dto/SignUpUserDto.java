c746dcda88938e12dcbf58ff515fe4cde0ec291a
Amitoshverm <amitosh.verma9@gmail.com>
2026-05-01 23:27:07 +0530

    validation added for signup

--- src/main/java/com/bookmarks/TypeAhead/dto/SignUpUserDto.java
+++ src/main/java/com/bookmarks/TypeAhead/dto/SignUpUserDto.java
@@ -1,9 +1,18 @@
 package com.bookmarks.TypeAhead.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
+
 public class SignUpUserDto {
 
+    @NotBlank
     private String username;
+    @NotBlank
+    @Email
     private String email;
+    @NotBlank
+    @Length(min = 6)
     private String password;
 
     public String getUsername() {
